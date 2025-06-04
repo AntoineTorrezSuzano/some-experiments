a767f0dcf6a3089f79fd2c436f882ac20f78f086
AntoineTorrezSuzano <142870032+AntoineTorrezSuzano@users.noreply.github.com>
2025-06-04 15:08:34 +0200

    Some changes

---
+++ flamebeginner/lib/touhou.dart
@@ -0,0 +1,10 @@
+import 'package:flame/game.dart';
+
+class Touhou extends FlameGame {
+  Touhou({super.children});
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+  }
+}
